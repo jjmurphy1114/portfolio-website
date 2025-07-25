d795a9b082118f47c364ad515aa2433d9a15e35b
Jacob Murphy <jjmurphy1114@outlook.com>
2025-07-25 16:52:31 -0400

    Changed github and linkedin pics

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,12 +5,12 @@ export const Footer = () => {
     <footer className="bg-lightGray text-black p-4 text-center">
       <div className="flex justify-center space-x-4 mb-2">
         <FooterButton
-          imgSrc="src/assets/github.svg"
+          imgSrc="/github.svg"
           text="GitHub"
           link="https://github.com/jjmurphy1114?tab=repositories"
         />
         <FooterButton
-          imgSrc="src/assets/linkedin.svg"
+          imgSrc="/linkedin.svg"
           text="LinkedIn"
           link="https://www.linkedin.com/in/jacobmurphy14"
         />
